ffec0c908a59121e2757cc828fb178140580841a
jarbasai <jarbasai@mailfence.com>
2023-03-25 03:54:36 +0000

    automations

--- scripts/remove_alpha.py
+++ scripts/remove_alpha.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(dirname(__file__)), "ovos_solver_unknown", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_solver_failure_plugin", "version.py")
 
 alpha_var_name = "VERSION_ALPHA"
 
